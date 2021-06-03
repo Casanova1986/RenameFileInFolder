7eb109a749f794f648c49fa7890ec92a37fa62a5
Casanova1986 <tudt.erp@gmail.com>
2021-06-04 01:12:19 +0700

    add

---
+++ trimBytes.py
@@ -0,0 +1,29 @@
+# Pythono3 code to rename multiple 
+# files in a directory or folder
+  
+# importing os module
+import os
+import sys
+
+# Function to rename multiple files
+def main():
+  print(sys.argv)
+  print(sys.argv[1])
+
+  for count, filename in enumerate(os.listdir(sys.argv[1])):
+    fp = open(sys.argv[1] + "\\" + filename, "rb")
+    data = fp.read()
+    fp.close()
+
+    fp = open(sys.argv[1] + "\\" + "_" + filename , "wb")
+    fp.write(data[1:-1])
+    fp.close()
+    print(count,filename)
+    
+       
+  
+# Driver Code
+if __name__ == '__main__':
+      
+    # Calling main() function
+    main()
